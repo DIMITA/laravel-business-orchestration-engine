0d228a848060005493b52b1d14a2b1e23ae72ad1
Dimita <dimi145dimi@gmail.com>
2024-06-15 13:00:00 +0200

    Add queue job for saga step execution: ExecuteSagaStep

---
+++ src/Jobs/ExecuteSagaStep.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Dimita\BusinessOrchestration\Jobs;
+
+use Dimita\BusinessOrchestration\Models\SagaStep;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class ExecuteSagaStep implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public SagaStep $step;
+
+    public function __construct(SagaStep $step)
+    {
+        $this->step = $step;
+    }
+
+    public function handle()
+    {
+        // Assume the step_name is a class that implements Executable
+        $stepClass = $this->step->step_name;
+        $instance = app($stepClass);
+        $instance->execute($this->step->saga->payload);
+    }
+}
